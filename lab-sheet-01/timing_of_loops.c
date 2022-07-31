e08fbef6cae7259f4bb7e36286df7db7f78640b0
Lian Remme <lirem101@hhu.de>
2022-07-31 15:12:31 +0800

    Extracted write into file into a method

--- lab-sheet-01/timing_of_loops.c
+++ lab-sheet-01/timing_of_loops.c
@@ -16,22 +16,30 @@ double* simple_loop(const int *iterations, int size) {
 }
 
 
+int write_into_file(char* filename, int n, int* iterations, double *simple_loop){
+    FILE *fpt;
+    fpt = fopen(filename, "w+");
+    if(fpt == NULL){
+        printf("Error while creating or opening file %s.\n", filename);
+        return 1;
+    }
+    fprintf(fpt,"Iterations, Simple_Loop [seconds]\n");
+    for (int i = 0; i < n; i++)
+    {
+        fprintf(fpt,"%d, %f\n", iterations[i], simple_loop[i]);
+    }
+    fclose(fpt);
+    return 0;
+}
+
+
 int main_timing() {
     int n = 5;
     int iterations[5] = {1000, 10000, 100000, 1000000, 10000000};
 
     double *times = simple_loop(iterations, n);
+    write_into_file("./lab-sheet-01/Timing.csv", n, iterations, times);
 
-    FILE *fpt;
-    fpt = fopen("./lab-sheet-01/Timing.csv", "w+");
-    fprintf(fpt,"Iterations, Simple_Loop [seconds]\n");
-
-    for (int i = 0; i < n; i++)
-    {
-        fprintf(fpt,"%d, %f\n", iterations[i], times[i]);
-    }
-
-    fclose(fpt);
     free(times);
     return 0;
 }
